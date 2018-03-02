f9cb1a14db6637aefc63fa733c895750bd49386e
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-03-01 22:33:56 -0800

    allows user to chose how many games will be played and tells how many are won

--- main.cpp
+++ main.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+int NumGames;
+int GamesWon;
+
 int main()
 {
   SecretDoor game;
-    
+
+  cout<<"How many times do you want to play?"<<endl;
+  cin>>NumGames;
+
+  for(int n=0; n <= NumGames; n++ )
+  {
+  game.newGame();
+  game.guessDoorC();
+  game.guessDoorC();
+    if(game.isWinner() == true)
+    {
+    GamesWon++;
+    }
+  }
+  cout<<"you won "<< GamesWon <<" games"<<endl;
   return 0;
 }
